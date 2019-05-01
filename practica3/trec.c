831b576f751f06a7398e7f8a61c8112a4b1c3ec0
pochy4545 <agustin.c.96@hotmail.com>
2019-05-01 18:48:37 -0300

    posibilidad distinta

--- practica3/trec.c
+++ practica3/trec.c
@@ -17,6 +17,35 @@ while (cadena1[largo]!='\0')
         posicion++;
     }
     largo++;
+    //si no puden usar is digit seria asi
+   /* switch (cadena1[largo])
+    {
+        case '0':cadena2[posicion]=cadena1[largo];
+        break;
+        case '1':cadena2[posicion]=cadena1[largo];
+        break;
+        case '2':cadena2[posicion]=cadena1[largo];
+        break;
+        case '3':cadena2[posicion]=cadena1[largo];
+        break;
+        case '4':cadena2[posicion]=cadena1[largo];
+        break;
+        case '5':cadena2[posicion]=cadena1[largo];
+        break;
+        case '6':cadena2[posicion]=cadena1[largo];
+        break;
+        case '7':cadena2[posicion]=cadena1[largo];
+        break;
+        case '8':cadena2[posicion]=cadena1[largo];
+        break;
+        case '9':cadena2[posicion]=cadena1[largo];
+        break;
+    default:
+        break;
+    }
+    posicion++;
+   */
+    
 }
 cadena2[posicion]='\0';
 printf("%s",cadena2);
